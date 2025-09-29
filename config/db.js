ee007f1bac70bddfcbd7381155e19f25a18387af
DevMaster7 <munnydevmaster@gmail.com>
2025-09-29 19:43:59 +0500

    mongoo db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const env = require("dotenv");
 
-
+env.config()
 function connectDB() {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log(`Connected to QuickCart Database`);
     });
 }
