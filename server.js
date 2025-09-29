f82aa8e2b8a4e9e651fcf846815d2d80b7a5baca
DevMaster7 <munnydevmaster@gmail.com>
2025-09-29 14:12:06 +0500

    port fixing

--- server.js
+++ server.js
@@ -327,6 +327,12 @@ app.use((req, res) => {
   res.status(404).render("PNF");
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
