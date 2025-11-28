613efcb2cb7bd79d3ec08dd3ff307c238afd0ed8
DevMaster7 <munnydevmaster@gmail.com>
2025-11-28 16:46:40 +0500

    changing sendotp method to resend

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -1,28 +1,55 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+// const env = require("dotenv");
+// const resend = require("resend")
+
+// env.config();
+// async function sendEmail(mail, html) {
+//   const resend = new Resend(process.env.RESEND_API_KEY);
+
+//   await resend.emails.send({
+//     from: `YuuCart ${process.env.EMAIL}`,
+//     to: `${mail}`,
+//     subject: "Your YuuCart OTP Code",
+//     html: `${html}`
+//   });
+//   // const transporter = nodemailer.createTransport({
+//   //   service: "gmail",
+//   //   auth: {
+//   //     user: process.env.EMAIL,
+//   //     pass: process.env.EMAIL_PASS,
+//   //   },
+//   // });
+
+//   // const mailOptions = {
+//   //   from: `"YuuCart " <${process.env.EMAIL}>`,
+//   //   to: `${mail}`,
+//   //   subject: "Your YuuCart OTP Code",
+//   //   html: `${html}`,
+//   // };
+
+//   // try {
+//   //   const info = await transporter.sendMail(mailOptions);
+//   // } catch (error) {
+//   //   console.error("Error sending email:", error);
+//   // }
+// }
+
+// module.exports = sendEmail;
+
+const { Resend } = require("resend");
 const env = require("dotenv");
 
 env.config();
+
 async function sendEmail(mail, html) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const mailOptions = {
-    from: `"YuuCart " <${process.env.EMAIL}>`,
-    to: `${mail}`,
+  await resend.emails.send({
+    from: "YuuCart <onboarding@resend.dev>",   // If domain not verified yet
+    to: mail,
     subject: "Your YuuCart OTP Code",
-    html: `${html}`,
-  };
-
-  try {
-    const info = await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
+    html: html
+  });
 }
 
 module.exports = sendEmail;
